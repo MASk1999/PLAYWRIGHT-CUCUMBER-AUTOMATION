bc5d5cf3fef73c998c79e27735f775dda9217a5e
MASk1999 <shaikhasad710@gmail.com>
2025-09-28 09:53:46 +0530

    Commit: Added tests for negative scenarios

--- tests/Heroku-login/features/heroku-login.feature
+++ tests/Heroku-login/features/heroku-login.feature
@@ -9,3 +9,16 @@ Scenario: Test successful login
     And I enter password
     When I click on the login button
     Then I see message Welcome to Secure Area
+
+Scenario: Test unsuccessful login using wrong password
+    Then I enter username
+    And I enter password as "some-wrong-password"
+    When I click on the login button
+    Then I see message Your password is invalid
+
+@ignore
+Scenario: Test unsuccessful login using wrong username  #---->This scenario will not run because of the ignore tag
+    Then I enter username as "Ruskin-Bond"
+    And I enter password as "ruskin-bond's-secret"
+    When I click on the login button
+    Then I see message your username is invalid!
--- tests/step-definitions.js
+++ tests/step-definitions.js
@@ -28,3 +28,21 @@ Then("I see message Welcome to Secure Area", async function() {
     const welcomeMessage = await this.page.getByText(/welcome to the secure area/i);
     await expect(welcomeMessage).toBeVisible();
 });
+
+Then("I enter password as {string}", async function(password) {
+    await loginPage.enterPassword(password);
+});
+
+Then("I see message Your password is invalid", async function() {
+    const errorMessage = await this.page.getByText(/your password is invalid!/i);
+    await expect(errorMessage).toBeVisible();
+});
+
+Then("I enter username as {string}", async function(username) {
+    await loginPage.enterUsername(username);
+});
+
+Then("I see message your username is invalid!", async function() {
+    const errorMessage = await this.page.getByText(/your username is invalid!/i);
+    await expect(errorMessage).toBeVisible();
+});
