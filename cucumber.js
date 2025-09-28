a6cc027dbb90baad89d4681ce3081709799fa37b
MASk1999 <shaikhasad710@gmail.com>
2025-09-28 08:53:26 +0530

    commit: configuring cucumber-js file

--- cucumber.js
+++ cucumber.js
@@ -1,10 +1,13 @@
 module.exports = {
     default: {
         require :[
-            // Add paths to step-definition files here
+            // Add paths to step-definition files and world here
+            './world.js',
+            './tests/step-definitions.js'
         ],
         paths: [
             // Add paths to feature files here
+            './tests/*/features/*.feature'
         ],
         format: ['html:reports/report.html'],    // Report-type
         parallel: 1,                // Number of scenarios to run in parallel
